b07c874214d788dd05ad4ab9f193e89ccfd8c3d0
schnurr <schnurr@st.ovgu.de>
2015-05-26 17:55:22 +0200

    Simple Evaluation

--- Image-Understanding02/Classification01/EvaluationUnit.cpp
+++ Image-Understanding02/Classification01/EvaluationUnit.cpp
@@ -7,6 +7,26 @@ EvaluationUnit::EvaluationUnit()
 }
 
 
-EvaluationUnit::~EvaluationUnit()
+EvaluationUnit::EvaluationUnit(std::vector<Image> TestImages)
 {
+	this->TestImages = TestImages;
+}
+
+double EvaluationUnit::EvaluateResultSimple(std::vector<std::string> &Result)
+{
+	assert(this->TestImages.size() == Result.size());
+
+	double correctClassifications = 0;
+	for (int iter = 0; iter < this->TestImages.size(); ++iter)
+	{
+		std::string* OriginalClass = &this->TestImages[iter].category;
+		std::string* ResultClass = &Result[iter];
+
+		if (*OriginalClass == *ResultClass)
+		{
+			correctClassifications++;
+		}
+	}
+	return correctClassifications / double(Result.size());
+
 }
--- Image-Understanding02/Classification01/EvaluationUnit.h
+++ Image-Understanding02/Classification01/EvaluationUnit.h
@@ -1,8 +1,17 @@
 #pragma once
+
+#include "Image.h"
+#include <assert.h> 
+#include <string>
+#include <vector>
+
 class EvaluationUnit
 {
+	std::vector<Image> TestImages;
+
 public:
 	EvaluationUnit();
-	~EvaluationUnit();
+	EvaluationUnit(std::vector<Image> TestImages);
+	double EvaluateResultSimple(std::vector<std::string> &Result);
 };
 
