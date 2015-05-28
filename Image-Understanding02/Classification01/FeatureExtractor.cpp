c185f00d64c48fa8b4fb271a075f22a27fa13435
schnurr <schnurr@st.ovgu.de>
2015-05-28 15:07:39 +0200

    Jetzt aber

--- Image-Understanding02/Classification01/FeatureExtractor.cpp
+++ Image-Understanding02/Classification01/FeatureExtractor.cpp
@@ -7,8 +7,16 @@ FeatureExtractor::FeatureExtractor()
 
 }
 
+FeatureExtractor::Parameters::Parameters()
+{
 	this->WindowX = 144;
 	this->WindowY = 144;
+}
+
+FeatureExtractor::~FeatureExtractor()
+{
+
+}
 
 void FeatureExtractor::computeHOGFeatures(std::vector<cv::Mat> &Images, std::vector<std::vector< cv::Mat >> &FeatureVectors)
 {
@@ -18,13 +26,14 @@ void FeatureExtractor::computeHOGFeatures(std::vector<cv::Mat> &Images, std::vec
 	std::vector< cv::Point > location;
 	std::vector< float > descriptors;
 	int Size = int(Images.size());
-	
+
 	for (int iter = 0; iter < Size; ++iter)
 	{
-
 		cv::cvtColor(Images[iter], gray, cv::COLOR_BGR2GRAY);
 		hog.compute(gray, descriptors, cv::Size(8, 8), cv::Size(0, 0), location);
 		std::cout << "  Feature Size: " + std::to_string(descriptors.size()) << std::endl;
-		FeatureVectors[iter].push_back(cv::Mat(descriptors).clone());		
+		FeatureVectors[iter].push_back(cv::Mat(descriptors).clone());
 	}
+}
+
 
