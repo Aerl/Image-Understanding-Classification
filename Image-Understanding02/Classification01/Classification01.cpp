49d90b976db25fd92536485f2b1345938f01929f
Julian Alpers <Julian.Alpers@st.ovgu.de>
2015-06-05 12:45:50 +0200

    FLANN Matcher

--- Image-Understanding02/Classification01/Classification01.cpp
+++ Image-Understanding02/Classification01/Classification01.cpp
@@ -18,6 +18,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	std::string path("../101_ObjectCategories");
 	ImageLoader LoadImages = ImageLoader(path);
 	FeatureExtractor GetFeatures;
+	DecisionMaker GetClassification;
 
 	std::vector<std::string> folders;
 	folders.push_back("accordion");
@@ -31,54 +32,68 @@ int _tmain(int argc, _TCHAR* argv[])
 	//LoadImages.LoadImages();
 	std::vector<cv::Mat> trainingImages;
 	std::vector<int> trainingLabels;
+
+	std::vector<cv::Mat> testImages;
+	std::vector<int> testLabels;
+
+	std::vector<int> classificationResults;
+
 	LoadImages.getTrainingData(trainingImages, trainingLabels);
+	LoadImages.getTrainingData(testImages, testLabels);
 
 	std::vector<std::vector< cv::Mat >> FeatureVectors = std::vector<std::vector< cv::Mat>>(trainingImages.size());
 	GetFeatures.computeHOGFeatures(trainingImages, FeatureVectors);
 
-	std::vector<std::vector< cv::Mat >> FeatureVectorsSURF = std::vector<std::vector< cv::Mat>>(trainingImages.size());
-	GetFeatures.computeSURFFeatures(trainingImages, FeatureVectorsSURF);
+	std::vector<std::vector< cv::Mat >> SURFTrain = std::vector<std::vector< cv::Mat>>(trainingImages.size());
+	GetFeatures.computeSURFFeatures(trainingImages, SURFTrain);
+	std::vector<std::vector< cv::Mat >> SURFTest = std::vector<std::vector< cv::Mat>>(trainingImages.size());
+	GetFeatures.computeSURFFeatures(trainingImages, SURFTest);
 
-	int num_files = trainingImages.size();
-	int img_area = 150*150;
-	cv::Mat labels(num_files, 1, CV_32FC1);
-	cv::Mat training_mat(num_files, img_area, CV_32FC1);
-	int labelIndex = 0;
+	GetClassification.MakeDecisionFLANN(SURFTrain, SURFTest, trainingLabels, classificationResults);
 
-	// reshape Images to one Mat for the SVM
-	for (cv::Mat img_mat : trainingImages)
-	{
-		int ii = 0;
-		for (int i = 0; i < img_mat.rows; i++) 
-		{
-			for (int j = 0; j < img_mat.cols; j++) 
-			{
-				training_mat.at<float>(labelIndex,ii++) = img_mat.at<uchar>(i, j);
-			}
-		}
-		labels.at<float>(labelIndex) = trainingLabels.at(labelIndex);
-		labelIndex++;
-	}
-	
-	// set up SVM parameters
-	CvSVMParams params;
-	params.svm_type = CvSVM::C_SVC;
-	params.kernel_type = CvSVM::LINEAR;
-	params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
-	//...etc
 
-	// set up Support Vector Machine for training and classification 
-	cv::SVM svm; 
-	svm.train(training_mat, labels, cv::Mat(), cv::Mat(), params);
 
-	// svm.predict to classify an image
-	std::vector<cv::Mat> testData;
-	std::vector<int> testLabels;
-	LoadImages.getTrainingData(testData, testLabels);
 
-	cv::Mat test2 = training_mat.row(18);
+	//int num_files = trainingImages.size();
+	//int img_area = 150*150;
+	//cv::Mat labels(num_files, 1, CV_32FC1);
+	//cv::Mat training_mat(num_files, img_area, CV_32FC1);
+	//int labelIndex = 0;
 
-	int p = svm.predict(test2);
+	//// reshape Images to one Mat for the SVM
+	//for (cv::Mat img_mat : trainingImages)
+	//{
+	//	int ii = 0;
+	//	for (int i = 0; i < img_mat.rows; i++) 
+	//	{
+	//		for (int j = 0; j < img_mat.cols; j++) 
+	//		{
+	//			training_mat.at<float>(labelIndex,ii++) = img_mat.at<uchar>(i, j);
+	//		}
+	//	}
+	//	labels.at<float>(labelIndex) = trainingLabels.at(labelIndex);
+	//	labelIndex++;
+	//}
+	//
+	//// set up SVM parameters
+	//CvSVMParams params;
+	//params.svm_type = CvSVM::C_SVC;
+	//params.kernel_type = CvSVM::LINEAR;
+	//params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+	////...etc
+
+	//// set up Support Vector Machine for training and classification 
+	//cv::SVM svm; 
+	//svm.train(training_mat, labels, cv::Mat(), cv::Mat(), params);
+
+	//// svm.predict to classify an image
+	//std::vector<cv::Mat> testData;
+	//std::vector<int> testLabels;
+	//LoadImages.getTrainingData(testData, testLabels);
+
+	//cv::Mat test2 = training_mat.row(18);
+
+	//int p = svm.predict(test2);
 
 
 
@@ -102,6 +117,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		//std::cout << "  class: " + iter->category << std::endl;
 		cv::waitKey(300);
 	}
+
 	/*
 	std::vector<Image> testImages = LoadImages.getTestImages();
 
--- Image-Understanding02/Classification01/DecisionMaker.h
+++ Image-Understanding02/Classification01/DecisionMaker.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <vector>
+#include <opencv2/opencv.hpp>
+
 class DecisionMaker
 {
+
+	struct Parameters
+	{
+		int ROIx;
+		int ROIy;
+		//! Default constructor ensuring that all variables are initialized.
+		Parameters();
+	};
+
+	Parameters parameters;
+
 public:
 	DecisionMaker();
+	DecisionMaker(int xDim, int yDim);
 	~DecisionMaker();
+	void MakeDecisionSVM(std::vector<std::vector< cv::Mat >> &features, std::vector<int> &trainingLabels, std::vector<int> &classificationResults);
+	void MakeDecisionFLANN(std::vector<std::vector< cv::Mat >> &featuresTrain, std::vector<std::vector< cv::Mat >> &featuresTest, std::vector<int> &trainingLabels, std::vector<int> &classificationResults);
 };
 
